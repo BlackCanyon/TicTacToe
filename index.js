8b92bb225aaaa65ebb15e1a14cc5ec1d5fa2af73
BlackCanyon <john@y2kdigital.com>
2020-12-06 22:17:39 -0600

    Initial commit. Implementing game logic.

---
+++ index.js
@@ -0,0 +1,138 @@
+//AI Logic (This implementation uses Minimax with no pruning because the search
+//space is so small.)
+function getMinimaxScore(board, depth, computerLetter, opponentLetter, maximixingPlayer) {
+  let bestValue = undefined;
+  if (isWinner(board, computerLetter) === true) {
+    return 1;
+  } else if (isWinner(board, opponentLetter) === true) {
+    return -1;
+  } else if (isBoardFull(board) === true) {
+    return 0;
+  }
+
+  if (maximixingPlayer === true) {
+    bestValue = -10;
+    let possibleMoves = findLegalMoves(board);
+    possibleMoves.forEach(function(move) {
+      let boardCopy = [...board];
+      makeMove(move, boardCopy, computerLetter);
+      v = getMinimaxScore(boardCopy, depth - 1, computerLetter, opponentLetter, false);
+      bestValue = Math.max(bestValue, v);
+    })
+  } else {
+    bestValue = 10;
+    let possibleMoves = findLegalMoves(board);
+    possibleMoves.forEach(function(move) {
+      let boardCopy = [...board];
+      makeMove(move, boardCopy, opponentLetter);
+      v = getMinimaxScore(boardCopy, depth - 1, computerLetter, opponentLetter, true);
+      bestValue = Math.min(bestValue, v);
+    });
+  }
+  return bestValue;
+}
+
+function getMinimaxMove(board, computerLetter, playerLetter) {
+  let bestScore = -2;
+  let bestMove = undefined;
+  let i = 0
+  let legalMoveArray = findLegalMoves(board)
+  legalMoveArray.forEach(function(move) {
+    let boardCopy = [...board];
+    makeMove(move, boardCopy, computerLetter);
+    score = getMinimaxScore(boardCopy, 9, computerLetter, playerLetter, false);
+
+    if (score > bestScore) {
+      bestMove = move;
+      bestScore = score;
+    }
+  })
+  return bestMove;
+}
+
+//helper functions
+
+function makeMove(move, board, letter) {
+  board[move] = letter;
+}
+
+function isWinner(b, l) {
+  if ((b[0] == l && b[1] == l && b[2] == l) || //horizontal first row
+    (b[3] == l && b[4] == l && b[5] == l) || //horizontal second row
+    (b[6] == l && b[7] == l && b[8] == l) || //horizontal third row
+    (b[0] == l && b[3] == l && b[6] == l) || //vertical first column
+    (b[1] == l && b[4] == l && b[7] == l) || //vertical middle column
+    (b[2] == l && b[5] == l && b[8] == l) || //vertical third column
+    (b[0] == l && b[4] == l && b[8] == l) || //diagonal top to bottom
+    (b[6] == l && b[4] == l && b[2] == l)) {
+    return true
+  } //diagonal bottom to top
+}
+
+function isBoardFull(board) {
+  for (let i = 0; i < 9; i++) {
+    if (isSpaceFree(board, i) === true) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function findLegalMoves(board) {
+  let legalMoves = [];
+  for (let i = 0; i < 9; i++) {
+    if (isSpaceFree(board, i) === true) {
+      legalMoves.push(i)
+    }
+  }
+  return legalMoves
+}
+
+function isSpaceFree(board, move) {
+  if (board[move] == ' ') {
+    return true;
+  }
+}
+
+//Main gameplay loop
+
+function startGame() {
+  if (gameStarted === false) {
+    let gameBoard = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+    let playerLetter = "X";
+    let computerLetter = "O";
+    let playerMove = undefined;
+    let computerMove = undefined;
+    gameStarted = true;
+
+    for (let i = 0; i < document.querySelectorAll(".tile").length; i++) {
+      document.querySelectorAll(".tile > button")[i].addEventListener("click", function() {
+        playerMove = this.parentElement.id;
+        if (isSpaceFree(gameBoard, playerMove) === true) {
+          makeMove(playerMove, gameBoard, playerLetter);
+          console.log(gameBoard);
+          if (isWinner(gameBoard, playerLetter) === true) {
+            console.log("You are the winner")
+          } else if (isBoardFull(gameBoard) == true) {
+            //Tie game stuff here.
+          } else {
+            computerMove = getMinimaxMove(gameBoard, computerLetter, playerLetter);
+            makeMove(computerMove, gameBoard, computerLetter);
+            console.log(gameBoard)
+            if (isWinner(gameBoard, computerLetter) === true) {
+              //Make some visual changes here.
+            } else if (isBoardFull(gameBoard) == true) {
+              //Tie game stuff here.
+            }
+          }
+        } else {
+          console.log("Don't cheat.")
+        }
+      })
+    }
+  }
+}
+
+let gameStarted = false;
+
+document.addEventListener("keydown", startGame)
